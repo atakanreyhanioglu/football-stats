41efc36682d5551fd173e14bc2bc02537feca7b6
atakanreyhanioglu <atakan.reyhanioglu@gmail.com>
2022-03-15 13:24:31 +0300

    html output added

--- build/index.js
+++ build/index.js
@@ -2,11 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
 const CsvFileReader_1 = require("./CsvFileReader");
-const ConsoleReport_1 = require("./reportTargets/ConsoleReport");
+// import {ConsoleReport} from "./reportTargets/ConsoleReport";
 const WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
 const Summary_1 = require("./Summary");
+const HtmlReports_1 = require("./reportTargets/HtmlReports");
 const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 const matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
-const summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
+// const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport())
+const summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new HtmlReports_1.HtmlReports());
 summary.buildAndPrintReport(matchReader.matches);
